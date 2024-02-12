18fe0750dd0221cadd426bb01e28c0dcefe1fa2b
Other_side <94060774+Bennett-Fourr@users.noreply.github.com>
2024-02-12 07:14:10 -0500

    Remove unused search engine configurations

--- src/content-script/search-engine-configs.ts
+++ src/content-script/search-engine-configs.ts
@@ -16,46 +16,16 @@ export const config: Record<string, SearchEngine> = {
     sidebarContainerQuery: ['#b_context'],
     appendContainerQuery: [],
   },
-  yahoo: {
-    inputQuery: ["input[name='p']"],
-    sidebarContainerQuery: ['#right', '.Contents__inner.Contents__inner--sub'],
-    appendContainerQuery: ['#cols', '#contents__wrap'],
-  },
   duckduckgo: {
     inputQuery: ["input[name='q']"],
     sidebarContainerQuery: ['.results--sidebar.js-results-sidebar'],
     appendContainerQuery: ['#links_wrapper'],
-  },
-  baidu: {
-    inputQuery: ["input[name='wd']"],
-    sidebarContainerQuery: ['#content_right'],
-    appendContainerQuery: ['#container'],
-    watchRouteChange(callback) {
-      const targetNode = document.getElementById('wrapper_wrapper')!
-      const observer = new MutationObserver(function (records) {
-        for (const record of records) {
-          if (record.type === 'childList') {
-            for (const node of record.addedNodes) {
-              if ('id' in node && node.id === 'container') {
-                callback()
-                return
-              }
-            }
-          }
-        }
-      })
-      observer.observe(targetNode, { childList: true })
-    },
   },
   kagi: {
     inputQuery: ["input[name='q']"],
     sidebarContainerQuery: ['.right-content-box._0_right_sidebar'],
     appendContainerQuery: ['#_0_app_content'],
   },
-  yandex: {
-    inputQuery: ["input[name='text']"],
-    sidebarContainerQuery: ['#search-result-aside'],
-    appendContainerQuery: [],
   },
   naver: {
     inputQuery: ["input[name='query']"],
