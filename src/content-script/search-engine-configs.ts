b980882e897a5e00c6718faaac22c4ac25b4f753
Other_side <otherside5@protonmail.com>
2024-03-04 22:03:32 -0500

    Remove outdated

--- src/content-script/search-engine-configs.ts
+++ src/content-script/search-engine-configs.ts
@@ -6,31 +6,6 @@ export interface SearchEngine {
 }
 
 export const config: Record<string, SearchEngine> = {
-  google: {
-    inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ['#rhs'],
-    appendContainerQuery: ['#rcnt'],
-  },
-  bing: {
-    inputQuery: ["[name='q']"],
-    sidebarContainerQuery: ['#b_context'],
-    appendContainerQuery: [],
-  },
-  duckduckgo: {
-    inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ['.results--sidebar.js-results-sidebar'],
-    appendContainerQuery: ['#links_wrapper'],
-  },
-  brave: {
-    inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ['infobox-snippet"'],
-    appendContainerQuery: [],
-  },
-  searx: {
-    inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ['#sidebar_results'],
-    appendContainerQuery: [],
-  },
   startpage: {
     inputQuery: ["input[name='query']"],
     sidebarContainerQuery: ['.sidebar-results'],
